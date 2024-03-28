91e10eaf2dda952a4c593bc78be96e2e6f1e218d
Uma Sahni <monkeytown64@gmail.com>
2024-03-28 12:14:52 +0530

    wallet

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -7,13 +7,29 @@ const Home = () => {
   return (
     <div className="flex-container">
       <div className="flex-home">
-        <Graph />
+        {/* Graph of population */}
+        <div>
+          <h1 style={{ marginBottom: "1rem" }}>
+            United States Population Data
+          </h1>
+          <Graph />
+        </div>
 
-        <div className="center-flex">
-          <img  src="metamask.svg" alt="" />
-          <WalletConnect />
+        {/* Connect Wallet */}
+        <div>
+          <h1 style={{ marginBottom: "1rem" }}>
+        
+            Connect Wallet with Mata Mask
+          </h1>
+          <div className="center-flex">
+            <img src="metamask.svg" alt="" />
+            <WalletConnect />
+          </div>
         </div>
       </div>
+
+      {/* Prices of coins */}
+      <h1 style={{ marginTop: "1rem" }}>CoinDesk Bitcoin Price</h1>
       <Prices />
     </div>
   );
